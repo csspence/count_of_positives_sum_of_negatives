a9953c9042414031123d8cf8336f2b0bd8ff3af8
Crew Spence <csspence123@gmail.com>
2020-03-08 05:14:07 -0500

    Wrote working solution for count_of_positives_sum_of_negatives.

--- count_of_positives_sum_of_negatives.js
+++ count_of_positives_sum_of_negatives.js
@@ -9,6 +9,26 @@ Example
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 */
 
-function countPositivesSumNegatives(input) {
-    return ?;
+const countPositivesSumNegatives = (input) => {
+  let pos = 0;
+  let neg = 0;
+  const arr = [];
+
+  if(input === null) {
+      return [];
+  }
+  if(input.length < 1) {
+      return [];
+  }
+  for(let i = 0; i < input.length; i++) {
+      if(input[i] < 0) {
+          neg += input[i];
+      }
+      if(input[i] > 0) {
+          pos++;
+      }
+  }
+
+  arr.push(pos, neg);
+  return arr;
 }
